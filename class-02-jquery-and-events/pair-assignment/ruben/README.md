1865f5cd06eabff9b6e3b837c65494f3aaf2a20d
Ruben Sarkisyan <rubens24@uw.edu>
2016-01-30 11:25:30 -0800

    completed class 02 pair assignment

---
+++ class-02-jquery-and-events/pair-assignment/ruben/README.md
@@ -0,0 +1,3 @@
+I know I am behind but I am working to be completely caught up by tomorrow if not the beginning of next week.
+
+The assignment took me a couple hours to complete and I am working on my own on this. 
---
+++ class-02-jquery-and-events/pair-assignment/ruben/scripts/articleView.js
@@ -0,0 +1,88 @@
+// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+var articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      // DONE: We need to take every author name from the page, and make it an option in the Author filter.
+      //       To do so, Build an `option` DOM element that we can append to the author select box.
+      //       Start by grabbing the author's name from `this` article element, and then use that bit of
+      //       text to create the option tag (in a variable named `optionTag`),
+      //       that we can append to the #author-filter select element.
+      //       YAY, DOM manipulation!
+      var val = $(this).find('address a').text();
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('#author-filter').append(optionTag);
+
+      // DONE: Similar to the above, but...
+      //       Avoid duplicates! We don't want to append the category name if the select
+      //       already has this category as an option!
+      val = $(this).attr('data-category');
+      optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+
+      $('article[data-author = "' + $(this).val() + '"]').fadeIn();
+
+    } else {
+      $('article').show();
+
+    }
+    $('#category-filter').val('');
+  });$('#category-filter').on('change', function () {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category = "'+ $(this).val() + '"]').fadeIn();
+    } else {
+      $('article').show();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+
+};
+
+articleView.handleMainNav = function() {
+  // TODO: Add an event handler to .main-nav element that will power the Tabs feature.
+  //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
+  //       single .tab-content section that is associated with the clicked .tab element.
+  //       So: You need to dynamically build a selector string with the correct ID, based on the
+  //       data available to you on the .tab element that was clicked.
+  $('.main-nav').on('click', '.tab', function () {
+    $('.tab-content').hide();
+    $('#' + $(this).attr('data-content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+};
+
+articleView.setTeasers = function() {
+  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
+
+  $('articles').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    $(this).parent().find('*').fadeIn();
+    $(this).hide();
+  })
+
+};
+
+// TODO: Call all of the above functions, once we are sure the DOM is ready.
+$(document).ready(function () {
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+});
