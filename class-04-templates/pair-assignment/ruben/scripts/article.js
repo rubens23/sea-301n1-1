2445e89c445e301cfb36dc0103baf45a7aead3a3
Ruben Sarkisyan <rubens24@uw.edu>
2016-02-02 19:09:34 -0800

    completed class 04 pair assignment

---
+++ class-04-templates/pair-assignment/ruben/scripts/article.js
@@ -0,0 +1,42 @@
+var articles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  var template = Handlebars.compile($('#articleHB').text());
+
+  // DONE: If your template will use properties that aren't on the object yet, add them.
+  //   Since your template can't hold any JS logic, we need to execute the logic here.
+  //   The result is added to the object as a new property, which can then be referenced by key in the template.
+  //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
+   return template(this);
+};
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+if (typeof rawData !== 'undefined') {
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+})
+
+rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+   });
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml())
+});
+}
